33c61c689b945bea8386f8b3fadf3fec09a5c5c2
GeoBrodas <geobro2310@gmail.com>
2021-08-11 19:53:40 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './style.css';
 
+import Data from '../mock-data.json';
+import { useState } from 'react';
+
 export default function App() {
+  const [query, setQuery] = useState('');
   return (
     <div className="app">
-      <input placeholder="Enter query" />
-      <p>Results</p>
+      <input
+        placeholder="Enter query"
+        onChange={event => setQuery(event.target.value)}
+      />
+      {Data.filter(data => {
+        if (query === '') {
+          return data;
+        } else if (data.author.toLowerCase().includes(query.toLowerCase())) {
+          return data;
+        }
+      }).map((data, key) => (
+        <p>{data.author}</p>
+      ))}
     </div>
   );
 }
