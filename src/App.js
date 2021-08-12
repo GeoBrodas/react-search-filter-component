053357c2f51aab7fe9c6616dab2a0fa09857412f
GeoBrodas <geobro2310@gmail.com>
2021-08-12 15:04:14 +0530

    add style

--- src/App.js
+++ src/App.js
@@ -10,16 +10,20 @@ export default function App() {
     <div className="app">
       <input
         placeholder="Enter query"
+        className="search-bar"
         onChange={event => setQuery(event.target.value)}
       />
       {Data.filter(data => {
         if (query === '') {
           return data;
-        } else if (data.author.toLowerCase().includes(query.toLowerCase())) {
+        } else if (data.title.toLowerCase().includes(query.toLowerCase())) {
           return data;
         }
       }).map((data, key) => (
-        <p>{data.author}</p>
+        <div className="box">
+          <p>{data.title}</p>
+          <p>{data.author}</p>
+        </div>
       ))}
     </div>
   );
